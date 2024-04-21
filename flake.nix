4b422ceacc1c99d4bd1b21d65526da502a7fabf4
sadanslargehole <sadan@sadanslargehole.com>
2024-04-21 19:57:10 -0400

    26 current  2024-04-21 19:57:03  24.05.20240419.5c24cf2       6.6.28                          *

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+    default = nixpkgs.lib.nixosSystem {
+    specialArgs = {inherit inputs;};
+    modules = [
+        ./boxes/desktop/configuration.nix
+        inputs.home-manager.nixosModules.default
+
+    ];
+    };
+    };
+    # nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+    #   specialArgs = {inherit inputs;};
+    #   modules = [
+    #     ./boxes/desktop/configuration.nix
+    #     inputs.home-manager.nixosModules.default
+    #   ];
+    # };
+  };
+}
