c1f55df4640f39c934312116a5217715e7ba5ebe
sadanslargehole <sadan@sadanslargehole.com>
2024-04-21 21:55:41 -0400

    30 current  2024-04-21 21:55:35  24.05.20240419.5c24cf2       6.6.28                          *

--- boxes/desktop/configuration.nix
+++ boxes/desktop/configuration.nix
@@ -86,6 +86,7 @@ nixpkgs.config.allowUnfree = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gcc
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     curl
     wget
