b79c667fb002198f563038dbe850c25b6bd9e561
sadanslargehole <sadan@sadanslargehole.com>
2024-04-23 17:22:06 -0400

    40 current  2024-04-23 17:22:00  24.05.20240419.5c24cf2       6.6.28                          *

--- boxes/desktop/home.nix
+++ boxes/desktop/home.nix
@@ -56,8 +56,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-  "testconfigfile".source = config.lib.file.mkOutOfStoreSymlink ../../dotfiles/testconfigfile;
-    # ".gitconfig".source = ../../dotfiles/.gitconfig;
+  # "testconfigfile".source = ../../dotfiles/testconfigfile;
+    ".gitconfig".source = ../../dotfiles/.gitconfig;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
