42151f742cc22d4b5fa2ddda1d55c8310dd425a4
sadanslargehole <sadan@sadanslargehole.com>
2024-05-10 23:48:28 -0400

    120 current  2024-05-10 23:48:17  24.05.20240419.5c24cf2       6.6.28                          *

--- boxes/desktop/home.nix
+++ boxes/desktop/home.nix
@@ -131,13 +131,15 @@ in
           fi
     '')
     (pkgs.writeShellScriptBin "install_eslint" ''
+      set -x
+      cp /home/${config.home.username}/.config/.eslintrc.json .
       pkgs=("@stylistic/eslint-plugin" "@typescript-eslint/eslint-plugin")
       if [[ -z $1 ]]; then
           echo please specify npm, pnpm, or yarn
           exit 1
       fi
       for i in "''${pkgs[@]}"; do
-        `$1 i $i`
+        `$1 i -D $i`
       done
     '')
   ];
