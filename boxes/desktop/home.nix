ee103fda842c5603179427f9d08a97f12e0ef189
sadanslargehole <sadan@sadanslargehole.com>
2024-05-12 18:44:19 -0400

    124 current  2024-05-12 18:44:05  24.05.20240419.5c24cf2       6.6.28                          *

--- boxes/desktop/home.nix
+++ boxes/desktop/home.nix
@@ -47,6 +47,8 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs;[
+    jq
+    lazygit
     protontricks
     thunderbird
     vlc
