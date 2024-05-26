4af1ae9060f9b206b3ce087caea082d969fff1a2
sadan <117494111+sadan4@users.noreply.github.com>
2024-05-26 11:17:43 -0400

    149 current  2024-05-26 11:17:29  24.05.20240419.5c24cf2       6.8.6-zen1                          *

--- boxes/desktop/configuration.nix
+++ boxes/desktop/configuration.nix
@@ -120,7 +120,6 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     curl
     wget
-    git
     ripgrep
     tldr
     gnupg
