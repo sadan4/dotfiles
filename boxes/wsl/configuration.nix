9bc5d1e49c8775c60e8a8b8ed1698f82f3eda23a
sadan <117494111+sadan4@users.noreply.github.com>
2024-11-26 17:35:15 -0500

    test

--- boxes/wsl/configuration.nix
+++ boxes/wsl/configuration.nix
@@ -31,7 +31,6 @@
     ddcutil
     i2c-tools
     fuse
-    gcc
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     curl
     wget
