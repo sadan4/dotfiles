a6a9b728ee36a02f2cb8de4aa2db75ffbc8beac2
sadan <117494111+sadan4@users.noreply.github.com>
2024-11-26 21:31:41 -0500

    fix

--- boxes/desktop/configuration.nix
+++ boxes/desktop/configuration.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed onconfiguraticonfig
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
+{config, pkgs, inputs, ...}:
 {
   imports =
     [
