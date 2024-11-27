0cf1044628ff3535c0a04332e89042bc18bfbd09
sadan <117494111+sadan4@users.noreply.github.com>
2024-11-26 20:18:33 -0500

    fix path

--- common/users/homeModules/dev/ide/default.nix
+++ common/users/homeModules/dev/ide/default.nix
@@ -1,6 +1,6 @@
 {pkgs, config, ...}:
 let 
-    pinned = import ../pinned.nix { inherit pkgs config; };
+    pinned = import ../../../../pinned.nix { inherit pkgs config; };
 in {
     home = {
         packages = with pkgs; [
