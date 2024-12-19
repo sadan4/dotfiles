8abe1f4ff90fd1ad235e15abda80b2804fd87c21
sadan <117494111+sadan4@users.noreply.github.com>
2024-12-19 00:57:04 -0500

    add gdb cofnig

--- common/users/homeModules/dev/cpp.nix
+++ common/users/homeModules/dev/cpp.nix
@@ -1,23 +1,29 @@
-{pkgs, unstable, ...}: {
-    home = {
-        packages = with pkgs; [
-            meson
-            autoPatchelfHook
-            cmake
-            # clang
-            libgcc
-            glib
-            bear
-            glibc
-            gdb
-            lldb_19
-            unstable.llvmPackages_19.clang-tools
-        ];
-        file = {
-            eslint_d_config = {
-                source = ../../../../dotfiles/eslintrc.json;
-                target = "./.config/.eslintrc.json";
-            };
-        };
+{ pkgs, unstable, ... }:
+{
+  home = {
+    packages = with pkgs; [
+      meson
+      autoPatchelfHook
+      cmake
+      # clang
+      libgcc
+      glib
+      bear
+      glibc
+      gdb
+      lldb_19
+      unstable.llvmPackages_19.clang-tools
+    ];
+    file = {
+      eslint_d_config = {
+        source = ../../../../dotfiles/eslintrc.json;
+        target = "./.config/.eslintrc.json";
+      };
+      gdb_config = {
+        source = ../../../../dotfiles/gdb;
+        target = "./.config/gdb";
+        recursive = true;
+      };
     };
+  };
 }
