f472a94e5d327a0e8572040bb6ee9076150a308a
sadan <117494111+sadan4@users.noreply.github.com>
2025-02-21 22:59:56 -0500

    server: add networkmanager, docker, nixHelper

--- common/users/meyer-server/default.nix
+++ common/users/meyer-server/default.nix
@@ -11,6 +11,9 @@ in
 {
   imports = [
     (import ../../systemModules/sops.nix { inherit NAME; })
+    (import ../../systemModules/networkManager.nix { inherit NAME; })
+    (import ../../systemModules/docker.nix { inherit NAME; })
+    (import ../../systemModules/nixHelper.nix { inherit NAME; })
   ];
   users = {
     users = {
