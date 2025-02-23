06ac170ef4e8002af2370ad41e30e90165c6d6fb
sadan <117494111+sadan4@users.noreply.github.com>
2025-02-23 13:10:16 -0500

    guhhhh why did i write yml

--- common/users/docker/vw/default.nix
+++ common/users/docker/vw/default.nix
@@ -7,7 +7,7 @@
     file = {
       vw = {
         source = ./docker_compose.yaml;
-        target = "./src/vw/docker_compose.yml";
+        target = "./src/vw/docker_compose.yaml";
       };
     };
   };
