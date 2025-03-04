b510e814fef7f725ddbeef82d896a3c1e9c5ccf3
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-04 15:04:49 -0500

    fix obsidian failing on large notes

--- common/users/docker/obsidian/nginx.nix
+++ common/users/docker/obsidian/nginx.nix
@@ -1,10 +1,12 @@
-
 { ... }:
 {
   services = {
     nginx = {
       virtualHosts = {
         "obsidian.sadan.zip" = {
+          extraConfig = ''
+            client_max_body_size 1000M;
+          '';
           forceSSL = true;
           useACMEHost = "sadan.zip";
           locations = {
