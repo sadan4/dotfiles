1c775baa48e4879960197dde2f174f684dbc9c23
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-09 19:30:25 -0400

    whoops! no ports pt. 2

--- common/systemModules/jellyfin.nix
+++ common/systemModules/jellyfin.nix
@@ -1,36 +1,38 @@
-{config, ...}: {
-    users = {
-        groups = {
-            media = {
-                gid = 1002;
+{ config, ... }:
+{
+  users = {
+    groups = {
+      media = {
+        gid = 1002;
+      };
+    };
+  };
+  services = {
+    jellyfin = {
+      dataDir = "${config.fileSystems."/storage".mountPoint}/jfData/";
+      group = "media";
+      enable = true;
+      openFirewall = true;
+    };
+  };
+  services = {
+    nginx = {
+      virtualHosts = {
+        "jf.sadan.zip" = {
+          forceSSL = true;
+          useACMEHost = "sadan.zip";
+          locations = {
+            "/" = {
+              proxyPass = "http://localhost:8096";
+              extraConfig = ''
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection $http_connection;
+              '';
             };
+          };
         };
+      };
     };
-    services = {
-        jellyfin = {
-            dataDir = "${config.fileSystems."/storage".mountPoint}/jfData/";
-            group = "media";
-            enable = true;
-        };
-    };
-    services = {
-        nginx = {
-            virtualHosts = {
-                "jf.sadan.zip" = {
-                    forceSSL = true;
-                    useACMEHost = "sadan.zip";
-                    locations = {
-                        "/" = {
-                            proxyPass = "http://localhost:8096";
-                            extraConfig = ''
-                                proxy_http_version 1.1;
-                                proxy_set_header Upgrade $http_upgrade;
-                                proxy_set_header Connection $http_connection;
-                            '';
-                        };
-                    };
-                };
-            };
-        };
-    };
+  };
 }
