1414a69553fe9d350427f8e2c387bd5c9ef03963
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-12 22:12:22 -0400

    forgot qbit nginx module

---
+++ common/users/docker/qbit/nginx.nix
@@ -0,0 +1,20 @@
+{ ... }:
+{
+  services = {
+    nginx = {
+      virtualHosts = {
+        "qbit.sadan.zip" = {
+          forceSSL = true;
+          useACMEHost = "sadan.zip";
+          locations = {
+            "/" = {
+              proxyPass = "http://localhost:3456";
+              proxyWebsockets = true;
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
