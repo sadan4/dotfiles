a423fe5be9772ae04b5befe7127ee55895cb24a5
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-19 18:15:13 -0400

    add completion for kitten

--- common/users/homeModules/terminal/kitty.nix
+++ common/users/homeModules/terminal/kitty.nix
@@ -7,12 +7,20 @@
     file = {
       kitty = {
         recursive = true;
-        source = ../../../dotfiles/kitty;
+        source = ../../../../dotfiles/kitty;
         target = "./.config/kitty";
       };
     };
     shellAliases = {
       "icat" = "kitten icat";
+    };
+  };
+  programs = {
+    zsh = {
+            # Completion support for kitten
+      initExtra = ''
+        compdef _kitty kitten
+      '';
     };
   };
 }
