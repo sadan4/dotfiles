51ab06be6df888cabb410a1733d58e20884f049e
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-19 23:35:57 -0400

    setup system tray

--- common/users/homeModules/displays/_makePanel.nix
+++ common/users/homeModules/displays/_makePanel.nix
@@ -58,7 +58,38 @@ builtins.map (section: baseConfig // section) [
     widgets = [
       {
         systemTray = {
-
+          icons = {
+            spacing = "small";
+          };
+          items = {
+            hidden = [
+              "flameshot"
+              # razer keybord lighting
+              "polychromatic-tray-applet"
+              "spotify-client"
+              # Electron apps all have the same id, see: https://github.com/electron/electron/issues/40936
+              # better to just filter them all out
+              "chrome_status_icon_1"
+              "org.kde.plasma.brightness"
+              # keyboard lang
+              "org.kde.plasma.keyboardlayout"
+              # caps lock, etc...
+              "org.kde.plasma.keyboardindicator"
+              # I would like for this to only show if sleep is being inhibited
+              # but it shows anytime the power profile is not the default
+              "org.kde.plasma.battery"
+              # I think this shows anytime you're saving clipboard history why???
+              "org.kde.plasma.clipboard"
+              # shows any time **any** media is playing or paused in the background
+              "org.kde.plasma.mediacontroller"
+              # always shown while notifications are enabled
+              "org.kde.plasma.notifications"
+            ];
+            shown = [
+              "org.kde.plasma.volume"
+              "org.kde.plasma.networkmanagement"
+            ];
+          };
         };
       }
       {
