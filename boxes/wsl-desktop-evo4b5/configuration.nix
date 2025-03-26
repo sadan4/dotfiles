fb813dd6fd744d2e4f52330334f2ce89ce623299
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-26 22:11:27 +0000

    set hostname

--- boxes/wsl-desktop-evo4b5/configuration.nix
+++ boxes/wsl-desktop-evo4b5/configuration.nix
@@ -5,6 +5,11 @@
 	wsl = {
 		defaultUser = "meyer";
 		enable = true;
+		wslConf = {
+			network = {
+				hostname = "wsl-desktop-evo4b5";
+			};
+		};
 	};
 	system.stateVersion = "24.05";
 	programs.zsh.enable = true;
