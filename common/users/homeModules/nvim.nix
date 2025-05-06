4c228c8e7803b4b00dc717cb0e2a6d5e94cb9b67
sadan <117494111+sadan4@users.noreply.github.com>
2025-05-06 17:23:01 -0400

    insane

--- common/users/homeModules/nvim.nix
+++ common/users/homeModules/nvim.nix
@@ -1,20 +1,31 @@
-{pkgs, lib, config, ...}: {
-    home = {
-        packages = with pkgs; [
-            neovim
-        ];
-        file = {
-            nvim = {
-                recursive = true;
-                source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/nvim";
-                target = "./.config/nvim";
-            };
-        };
-        sessionVariables = {
-            EDITOR = "nvim";
-            MANPAGER = "nvim +Man!";
-            # readonly, quit on q
-            PAGER = "nvim -R +\"nmap q <CMD>q!<CR>\"";
-        };
+{
+  pkgs,
+  config,
+  ...
+}:
+let
+  pager_cmd_var = ''nvim -R +\"nmap q <CMD>q\!<CR>\"'';
+  pager_cmd_alias = ''nvim -R +"nmap q <CMD>q!<CR>"'';
+in
+{
+  home = {
+    packages = with pkgs; [
+      neovim
+    ];
+    file = {
+      nvim = {
+        recursive = true;
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/nvim";
+        target = "./.config/nvim";
+      };
     };
+    sessionVariables = {
+      EDITOR = "nvim";
+      MANPAGER = "nvim +Man!";
+      PAGER = pager_cmd_var;
+    };
+    shellAliases = {
+      pager = pager_cmd_alias;
+    };
+  };
 }
