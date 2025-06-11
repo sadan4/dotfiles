b7966b86db6c151cc1ff06b7fa6c5421f93e36de
sadan <117494111+sadan4@users.noreply.github.com>
2025-06-11 14:14:38 -0400

    update flakeify

--- common/users/homeModules/scripts/flakeify.sh
+++ common/users/homeModules/scripts/flakeify.sh
@@ -1,5 +1,6 @@
 if [ ! -e flake.nix ]; then
-    nix flake new -t github:nix-community/nix-direnv .
+    nix flake new -t github:sadan4/direnvTemplate .
+    direnv allow
 elif [ ! -e .envrc ]; then
     echo "use flake" > .envrc
     direnv allow
