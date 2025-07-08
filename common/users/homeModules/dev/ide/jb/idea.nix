47f13c67aa60e8a0b78f42776d1c6615786f7071
sadan <117494111+sadan4@users.noreply.github.com>
2025-07-08 00:45:13 -0400

    move intellij to a FHSEnv

--- common/users/homeModules/dev/ide/jb/idea.nix
+++ common/users/homeModules/dev/ide/jb/idea.nix
@@ -1,10 +1,36 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   imports = [
     ../../../pinned.nix
   ];
   home = {
-    packages = with pkgs.pinned; [
-      idea-ultimate
-    ];
+    packages =
+      let
+        ij = pkgs.pinned.idea-ultimate;
+      in
+      with pkgs;
+      [
+        (buildFHSEnv {
+          name = "idea-ultimate";
+          inherit (ij) version meta;
+          targetPkgs = _: [
+            ij
+            libGL
+            freetype
+            fontconfig
+            xorg.libXi
+            xorg.libX11
+            xorg.libXtst
+            xorg.libXrender
+            xorg.libXext
+            libappindicator-gtk3
+          ];
+          runScript = lib.getExe ij;
+          extraInstallCommands = ''
+            mkdir -p $out/share/applications
+            cp ${ij}/share/applications/idea-ultimate.desktop $out/share/applications/idea-ultimate.desktop
+          '';
+        })
+      ];
   };
 }
