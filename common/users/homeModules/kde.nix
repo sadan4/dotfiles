31cb1c7180ebda44f839e167d604f21885b4b92e
sadan <117494111+sadan4@users.noreply.github.com>
2025-07-08 00:45:25 -0400

    add xkill to kde keybinds

--- common/users/homeModules/kde.nix
+++ common/users/homeModules/kde.nix
@@ -1,17 +1,32 @@
-{pkgs, ...}: {
-    imports = [
-        ./plasma.nix
+{ pkgs, ... }:
+{
+  imports = [
+    ./plasma.nix
+  ];
+  home = {
+    packages = with pkgs; [
+      kdePackages.filelight
+      kdePackages.ksshaskpass
+      xsel
+      xorg.xkill
+      kdePackages.kcolorchooser
+      qalculate-qt
+      gnome-disk-utility
+      kdePackages.plasma-browser-integration
+      xmacro
     ];
-    home = {
-        packages = with pkgs; [
-            kdePackages.filelight
-            kdePackages.ksshaskpass
-            xsel
-            kdePackages.kcolorchooser
-            qalculate-qt
-            gnome-disk-utility
-            kdePackages.plasma-browser-integration
-            xmacro
-        ];
+  };
+  programs = {
+    plasma = {
+      hotkeys = {
+        commands = {
+          "xkill" = {
+            name = "xkill";
+            key = "Ctrl+Shift+K";
+            command = "xkill";
+          };
+        };
+      };
     };
+  };
 }
