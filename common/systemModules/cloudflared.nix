b8efeeeacc2949f5caff8ec877b3ff0d5ea80310
sadan <117494111+sadan4@users.noreply.github.com>
2025-07-13 02:35:51 -0400

    finish cloudflared config

--- common/systemModules/cloudflared.nix
+++ common/systemModules/cloudflared.nix
@@ -1,20 +1,39 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  environment = {
+    systemPackages = with pkgs; [
+        cloudflared
+    ];
+  };
   sops = {
     secrets = {
       cfd = {
         format = "binary";
         sopsFile = ./cloudflared.pem;
       };
+      cfdt = {
+        format = "binary";
+        sopsFile = ./cloudflared.conf;
+      };
+    };
+  };
+  boot = {
+    kernel = {
+      sysctl = {
+        "net.core.rmem_max" = 7500000;
+        "net.core.wmem_max" = 7500000;
+      };
     };
   };
   services = {
     cloudflared = {
+      enable = true;
       tunnels = {
-        "00000000-0000-0000-0000-000000000000" = {
+        "7bb2884d-fa49-4c9e-893a-81804662582b" = {
+          credentialsFile = config.sops.secrets.cfdt.path;
           default = "http_status:404";
           ingress = {
-            "*.sadan.zip" = "http://localhost:80";
+            "jf.sadan.zip" = "http://localhost:80";
           };
         };
       };
