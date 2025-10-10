7bf8f5ed6d5a43f049306ed0423a0ab626b62726
sadan <117494111+sadan4@users.noreply.github.com>
2025-10-09 20:34:18 -0400

    enable sysrq and add earlyoom

--- common/systemModules/kernel.nix
+++ common/systemModules/kernel.nix
@@ -1,5 +1,17 @@
-{pkgs, ...}: {
-    boot = {
-        kernelPackages = pkgs.linuxPackages_6_16;
+{ pkgs, ... }:
+{
+  boot = {
+    kernelPackages = pkgs.linuxPackages_6_16;
+    kernel = {
+      sysctl = {
+        "kernel.sysrq" = 1;
+      };
     };
+  };
+  services = {
+    earlyoom = {
+      enable = true;
+      enableNotifications = true;
+    };
+  };
 }
