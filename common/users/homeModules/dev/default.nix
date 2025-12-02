92b3890443fec9604c62f86860c97a5baf3bdbda
sadan <117494111+sadan4@users.noreply.github.com>
2025-12-01 20:14:57 -0500

    add unstable copilot cli

--- common/users/homeModules/dev/default.nix
+++ common/users/homeModules/dev/default.nix
@@ -1,4 +1,7 @@
 {pkgs, ...}: {
+	imports = [
+		../unstable.nix
+	];
 	home = {
 		packages = with pkgs; [
 			gnumake
@@ -12,6 +15,7 @@
 			man-pages
 			d-spy
 			hyperfine
+			unstable.github-copilot-cli
 		];
 		shellAliases = {
 			cloc = "${pkgs.tokei}/bin/tokei";
