f66de73050de883f44a5c51124cabbed08b621c3
sadan <117494111+sadan4@users.noreply.github.com>
2025-12-01 20:22:49 -0500

    use clion EAP

--- common/users/homeModules/dev/ide/jb/clion.nix
+++ common/users/homeModules/dev/ide/jb/clion.nix
@@ -1,7 +1,25 @@
-{pkgs, ...}: {
-	home = {
-		packages = with pkgs.jetbrains; [
-			clion
-		];
-	};
+{ pkgs, ... }:
+let
+  # https://www.jetbrains.com/clion/nextversion
+  version = "253.28294.254";
+	# https://download.jetbrains.com/cpp/CLion-253.28294.254.tar.gz
+  url = "https://download.jetbrains.com/cpp/CLion-${version}.tar.gz";
+  sha256 = "sha256-N5SFt31GGZ8uq0LK9h2LFTwo1SdNzEuUOFz3jLvsmq8=";
+in
+{
+  imports = [
+    ../../../unstable.nix
+  ];
+  home = {
+    packages = with pkgs.unstable.jetbrains; [
+      (clion.overrideAttrs (
+        _: _: {
+          inherit version;
+          src = pkgs.fetchurl {
+            inherit url sha256;
+          };
+        }
+      ))
+    ];
+  };
 }
