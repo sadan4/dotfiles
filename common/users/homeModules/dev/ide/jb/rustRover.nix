c6fc9dc0a392efafa34f6fc4e223fca68cdd701a
sadan <117494111+sadan4@users.noreply.github.com>
2025-12-03 18:22:53 -0500

    use rustrover EAP

--- common/users/homeModules/dev/ide/jb/rustRover.nix
+++ common/users/homeModules/dev/ide/jb/rustRover.nix
@@ -1,11 +1,24 @@
 { pkgs, ... }:
+let
+  # https://www.jetbrains.com/rust/nextversion/
+  version = "253.28294.188";
+  url = "https://download.jetbrains.com/rustrover/RustRover-${version}.tar.gz";
+  sha256 = "sha256-X4AC9E/40bb4jnxKj912yJiY5vz2EZXf53qNS+Wv3ZE=";
+in
 {
   imports = [
     ../../../unstable.nix
   ];
   home = {
     packages = with pkgs.unstable.jetbrains; [
-      rust-rover
+      (rust-rover.overrideAttrs (
+        _: _: {
+          inherit version;
+          src = pkgs.fetchurl {
+            inherit url sha256;
+          };
+        }
+      ))
     ];
   };
 }
