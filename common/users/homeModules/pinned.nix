22187868110732641d769eb2e364d88635a277c6
sadan <117494111+sadan4@users.noreply.github.com>
2025-12-10 18:14:23 -0500

    fix pinned.clang-tools
    somehow didn't notice this was missing

--- common/users/homeModules/pinned.nix
+++ common/users/homeModules/pinned.nix
@@ -42,7 +42,7 @@
 							(fetch {
 									url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
 									sha256 = "sha256:0m0xmk8sjb5gv2pq7s8w7qxf7qggqsd3rxzv3xrqkhfimy2x7bnx";
-								}).clang-tools;
+								}).llvmPackages_20.clang-tools;
 						# support non-updated plugins
 						idea-ultimate =
 							(fetch {
