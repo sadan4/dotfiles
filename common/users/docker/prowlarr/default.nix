1d05ad89578bb84809682af2649b8991a8c6b7df
sadan <117494111+sadan4@users.noreply.github.com>
2025-12-11 19:12:10 -0500

    fix prowlarr

--- common/users/docker/prowlarr/default.nix
+++ common/users/docker/prowlarr/default.nix
@@ -1,12 +1,17 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
+let 
+    dataDir = "/storage/prowlarrConf";
+in
 {
   imports = [
     ./nginx.nix
   ];
+  # I would just use services.prowlarr, but the implementation in nixpkgs is evil and 
+  # loves randomly changing ownership of it's data files, erroring the server
   services = {
     prowlarr = {
-      enable = true;
-      dataDir = "/storage/prowlarrConf";
+      enable = false;
+      inherit dataDir;
     };
   };
   users = {
@@ -23,11 +28,36 @@
   systemd = {
     services = {
       prowlarr = {
+        description = "Prowlarr";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        environment = {
+            PROWLARR__LOG__ANALYTICSENABLED = "false";
+            PROWLARR__SERVER__PORT = "9696";
+            PROWLARR__UPDATE__AUTOMATICALLY = "false";
+            PROWLARR__UPDATE__MECHANISM = "external";
+        };
         serviceConfig = {
-          Group = "media";
-          User = config.users.users.prowlarr.name;
+            Type = "simple";
+            DynamicUser = false;
+            User = config.users.users.prowlarr.name;
+            ExecStart = "${lib.getExe pkgs.prowlarr} -nobrowser -data=${dataDir}";
+            Restart = "on-failure";
         };
       };
+    };
+    tmpfiles = {
+        settings = {
+            "10-prowlarr" = {
+                "${dataDir}" = {
+                    Z = {
+                        user = config.users.users.prowlarr.name;
+                        group = config.users.groups.media.name;
+                        mode = "-";
+                    };
+                };
+            };
+        };
     };
   };
 }
