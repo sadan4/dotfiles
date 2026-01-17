e930d36c5512cb6764458a94474f62b68901981d
sadan <117494111+sadan4@users.noreply.github.com>
2026-01-17 04:18:03 -0500

    use custom sqlite for sonarr

--- common/users/docker/sonarr/default.nix
+++ common/users/docker/sonarr/default.nix
@@ -1,9 +1,14 @@
-{...}: {
+{pkgs, ...}: 
+let 
+	sqlite = pkgs.callPackage ../../../../customPackages/sqlite {};
+	sonarr = pkgs.sonarr.override { inherit sqlite; };
+in{
 	imports = [
 		./nginx.nix
 	];
 	services = {
 		sonarr = {
+			package = sonarr;
 			enable = true;
 			group = "media";
 			dataDir = "/storage/sonarrConf";
