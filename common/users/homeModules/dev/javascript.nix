c559fe62debeb82c2818af9994fb6d4e4f9a8770
sadan <117494111+sadan4@users.noreply.github.com>
2026-02-03 14:44:56 -0500

    setup global pnpm store

--- common/users/homeModules/dev/javascript.nix
+++ common/users/homeModules/dev/javascript.nix
@@ -1,6 +1,7 @@
-{pkgs, ...}: let
+{pkgs, config, ...}: let
 	# use unstable nodejs because of nodejs/node#60580
 	node = pkgs.unstable.nodejs_25;
+	PNPM_HOME = "/home/${config.home.username}/.local/pnpm-home";
 in {
 	imports = [
 		../prisma.nix
@@ -10,11 +11,14 @@ in {
 	];
 	programs = {
 		zsh = {
-			initContent = ''
-				eval "$(${node}/bin/node --completion-bash)"
-				eval "$(${node}/bin/npm completion)"
-				eval "$(${pkgs.unstable.pnpm}/bin/pnpm completion zsh)"
-			'';
+			initContent =
+				# sh
+				''
+					eval "$(${node}/bin/node --completion-bash)"
+					eval "$(${node}/bin/npm completion)"
+					eval "$(${pkgs.unstable.pnpm}/bin/pnpm completion zsh)"
+					[[ -d ${PNPM_HOME} ]] || mkdir -p ${PNPM_HOME}
+				'';
 		};
 	};
 	home = {
@@ -22,6 +26,12 @@ in {
 			pd = "/home/meyer/dev/ts/pnpm/pnpm/dev/pd.js";
 			webpack = "webpack-cli";
 			eslintd = "eslint_d";
+		};
+		sessionVariables = {
+			inherit PNPM_HOME;
+		};
+		sessionSearchVariables = {
+			PATH = [PNPM_HOME];
 		};
 		packages = with pkgs;
 			[
