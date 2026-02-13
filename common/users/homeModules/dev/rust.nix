92803cebdc2b8d031291d39020d893634cd69256
sadan <117494111+sadan4@users.noreply.github.com>
2026-02-13 17:26:07 -0500

    improve rustup completion generation command

--- common/users/homeModules/dev/rust.nix
+++ common/users/homeModules/dev/rust.nix
@@ -23,15 +23,9 @@ in {
 	};
 	programs = {
 		zsh = {
-			# Few horror things here
-			# 1. rustup's completion is broken for zsh. See: https://github.com/rust-lang/rustup/issues/2268
-			# 2. patch cant read the source file from stdin, so tmpfiles have to be used
+			# rustup's completion is broken for zsh. See: https://github.com/rust-lang/rustup/issues/2268
 			initContent = ''
-				TMP_RUSTUP_FILE=$(mktemp)
-				${rustup}/bin/rustup completions zsh > "$TMP_RUSTUP_FILE"
-				eval "$(patch -s -o - -i ${./rustupCompPatch.diff} $TMP_RUSTUP_FILE)"
-				rm $TMP_RUSTUP_FILE
-				unset TMP_RUSTUP_FILE
+				eval "$(patch -s -o - -i ${./rustupCompPatch.diff} =(${rustup}/bin/rustup completions zsh))"
 			'';
 		};
 	};
